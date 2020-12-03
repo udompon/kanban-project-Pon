e54f686c24fcb5a25e6ea0a57337d2dafa5ad064
Pon <ponnaruk41@hotmail.com>
2020-12-03 17:41:33 +0700

    123

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <Kanban :data="data" :create_task_submit=" create_task_submit"/>
+    <Kanban
+      :data="data"
+      :create_task_submit="create_task_submit"
+      :move_item_task="move_item_task"
+    />
   </div>
 </template>
 
@@ -12,39 +16,51 @@ export default {
   components: {
     Kanban,
   },
-  data (){
-    return{
-      data:[
+  data() {
+    return {
+      data: [
         {
-          name:"todo",
-          color:"#84FBF7",
-          tasks:[{
-            task_name: "tesk"
-          }]
+          name: "todo",
+          color: "#84FBF7",
+          tasks: [
+            {
+              task_name: "test",
+            },
+          ],
         },
         {
-          name:"doing",
-          color:"#C0FB84",
-          tasks:[{
-            task_name: "tesk"
-          }]
+          name: "doing",
+          color: "#C0FB84",
+          tasks: [
+            {
+              task_name: "test",
+            },
+          ],
         },
         {
-          name:"done",
-          color:"#F69D9D",
-          tasks:[{
-            task_name: "tesk"
-          }]
+          name: "done",
+          color: "#F69D9D",
+          tasks: [
+            {
+              task_name: "test",
+            },
+          ],
         },
-      ]
-    }
+      ],
+    };
   },
-  methods:{
-    create_task_submit(column_index,item){
-      this.data[column_index].tasks.push(item)
+  methods: {
+    create_task_submit(column_index, item) {
+      this.data[column_index].tasks.push(item);
+    },
+    move_item_task(column_a, index_task_a, column_b, index_task_b) {
+      var item = this.data[column_a].tasks[index_task_a];
 
-    }
-  }
+      this.data[column_a].tasks.splice(index_task_a, 1);
+
+      this.data[column_b].tasks.splice(index_task_b, 0,item);
+    },
+  },
 };
 </script>
 
