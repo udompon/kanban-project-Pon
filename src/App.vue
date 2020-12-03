cd9754bdc86a6492b338bfdcf5dad933fe1a3370
Pon <ponnaruk41@hotmail.com>
2020-12-03 14:02:50 +0700

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Kanban />
+    <Kanban :data="data"/>
   </div>
 </template>
 
@@ -12,6 +12,27 @@ export default {
   components: {
     Kanban,
   },
+  data (){
+    return{
+      data:[
+        {
+          name:"todo",
+          color:"#84FBF7",
+          tasks:[]
+        },
+        {
+          name:"doing",
+          color:"#C0FB84",
+          tasks:[]
+        },
+        {
+          name:"done",
+          color:"#F69D9D",
+          tasks:[]
+        },
+      ]
+    }
+  }
 };
 </script>
 
--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="kanban">Kanban</div>
+  <div class="kanban">
+    <div
+      class="culumn"
+      :style="{ backgroundColor: column.color }"
+      v-for="(column, index) in data"
+      :key="index"
+    >
+      <div class="colum-header">
+        {{ column.name }}
+      </div>
+      <div class="column-body">
+        <div class="create-task" @click="create_task(index)">Create task</div>
+      </div>
+    </div>
+    <b-modal ref="create-task-modal" title="Create Task">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    data: Array,
+  },
+  methods: {
+    create_task(index_column) {
+      console.log(index_column);
+      this.$refs["create-task-modal"].show();
+    },
+  },
+};
 </script>
 
 <style>
@@ -11,5 +38,40 @@ export default {};
   width: 100%;
   height: 100%;
   background-color: rgb(210, 162, 255);
+}
+.culumn {
+  height: 600px;
+  width: 300px;
+  border-radius: 30px;
+  display: inline-block;
+  margin: 25px;
+  -webkit-box-shadow: 21px 21px 18px -7px rgba(0, 0, 0, 0.61);
+  -moz-box-shadow: 21px 21px 18px -7px rgba(0, 0, 0, 0.61);
+  box-shadow: 21px 21px 18px -7px rgba(0, 0, 0, 0.61);
+  padding: 18px;
+}
+.colum-header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  font-size: 32px;
+  font-weight: bold;
+}
+.column-body {
+  height: calc(100% - 50px);
+  border-radius: 10px;
+  padding: 5px;
+  background-color: #b9c1f0a2;
+}
+.create-task {
+  widows: 100%;
+  height: auto;
+  padding: 10px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+.create-task:hover {
+  background-color: cornflowerblue;
 }
 </style>
